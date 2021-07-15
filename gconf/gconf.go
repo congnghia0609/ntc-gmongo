5fc53f335ca5dcce7b962ddb76b5a4c3b876f512
nghiatc <congnghia0609@gmail.com>
2021-07-15 14:56:40 +0700

    add gconf

---
+++ gconf/gconf.go
@@ -0,0 +1,55 @@
+package gconf
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+var config *viper.Viper
+var env string
+
+func Init(wdir string) {
+	//// init Configuration
+	environment := flag.String("e", "development", "run project with mode [-e development | test | production]")
+	flag.Usage = func() {
+		fmt.Println("Usage: ./[appname] -e development | test | production")
+		os.Exit(1)
+	}
+	flag.Parse()
+	log.Printf("============== GConf environment: %s", *environment)
+	InitEnv(wdir, *environment)
+}
+
+// Init is an exported method that takes the environment starts the viper (external lib) and
+// returns the configuration struct.
+func InitEnv(wdir string, environment string) {
+	log.Printf("============== GConfig Init Environment: %s ==============", environment)
+	var err error
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.SetConfigName(environment)
+	v.AddConfigPath(filepath.Join(wdir, "conf"))
+	err = v.ReadInConfig()
+	if err != nil {
+		log.Fatal("error on parsing configuration file")
+	}
+	config = v
+	env = environment
+}
+
+func JoinPath(basedir string, path string) string {
+	return filepath.Join(basedir, path)
+}
+
+func GetConfig() *viper.Viper {
+	return config
+}
+
+func GetEnv() string {
+	return env
+}
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/congnghia0609/ntc-gmongo/gconf"
+)
+
+func InitGConf() {
+	_, b, _, _ := runtime.Caller(0)
+	wdir := filepath.Dir(b)
+	fmt.Println("wdir:", wdir)
+	gconf.Init(wdir)
+}
 
 func main() {
+	InitGConf()
+
 	fmt.Println("Hello world...")
+
 }
