c3bbbbb44be0eea5015a476bc01c21fa3f970efb
nghiatc <congnghia0609@gmail.com>
2021-10-31 00:41:06 +0700

    add IncrementCounter

--- main.go
+++ main.go
@@ -28,13 +28,38 @@ func main() {
 	gmongo.InitMongo()
 	defer gmongo.MClose()
 
-	ida, _ := mid.GetNext("aaa")
-	fmt.Println("ida =", ida)
-	// rs, _ := mid.ResetID("aaa", 0)
-	// fmt.Println("rs =", rs)
+	//ida, _ := mid.GetNext("aaa")
+	//fmt.Println("ida =", ida)
+	//// rs, _ := mid.ResetID("aaa", 0)
+	//// fmt.Println("rs =", rs)
+	//gida, _ := mid.GetID("aaa")
+	//fmt.Println("gida =", gida)
+	//
+	//idb, _ := mid.GetNext("bbb")
+	//fmt.Println("idb =", idb)
+	//gidb, _ := mid.GetID("bbb")
+	//fmt.Println("gidb =", gidb)
 
-	idb, _ := mid.GetNext("bbb")
-	fmt.Println("idb =", idb)
+	// get id not exits
+	gidNotExits, err := mid.GetID("gidNotExits")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("gidNotExits =", gidNotExits)
+
+	//// Test counter increment
+	//c1, _ := mid.IncrementCounter("counter1", 5)
+	//fmt.Println("c1 =", c1)
+	//gc1, _ := mid.GetID("counter1")
+	//fmt.Println("gc1 =", gc1)
+	//// Test counter decrement
+	//c2, _ := mid.IncrementCounter("counter1", -8)
+	//fmt.Println("c2 =", c2)
+	//gc2, _ := mid.GetID("counter1")
+	//fmt.Println("gc2 =", gc2)
+	//
+	//c3, _ := mid.IncrementCounter("counter3", 5)
+	//fmt.Println("c3 =", c3)
 
 	// Hang thread Main.
 	c := make(chan os.Signal, 1)
--- mid/midgen.go
+++ mid/midgen.go
@@ -33,7 +33,6 @@ func GetNext(name string) (int64, error) {
 	upsert := true
 	after := options.After
 	opt := options.FindOneAndUpdateOptions{ReturnDocument: &after, Upsert: &upsert}
-
 	collection := gmongo.TestDB.Collection(tableName)
 	err := collection.FindOneAndUpdate(context.Background(), filter, update, &opt).Decode(&idgen)
 	// log.Println(err)
@@ -45,27 +44,51 @@ func GetNext(name string) (int64, error) {
 			return 0, err
 		}
 	}
-
-	// jidgen, _ := json.Marshal(idgen)
-	// fmt.Printf("idgen = %s\n", string(jidgen))
-	// fmt.Println(idgen)
-
 	return idgen.Seq, nil
 }
 
 // ResetID reset id gen to value
 func ResetID(id string, value int64) (int64, error) {
 	collection := gmongo.TestDB.Collection(tableName)
-
 	opts := options.Update().SetUpsert(true)
 	filter := bson.D{{"_id", id}}
 	update := bson.D{{"$set", bson.D{{"seq", value}}}}
-
 	result, err := collection.UpdateOne(context.Background(), filter, update, opts)
 	if err != nil {
 		return 0, err
 	}
 	// log.Println("nid:", nid)
-
 	return result.MatchedCount, nil
+}
+
+// GetID get value of id
+func GetID(id string) (int64, error) {
+	mid := MIdGen{}
+	collection := gmongo.TestDB.Collection(tableName)
+	filter := bson.D{{"_id", id}}
+	err := collection.FindOne(context.Background(), filter).Decode(&mid)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return 0, nil
+		} else {
+			return 0, err
+		}
+	}
+	return mid.Seq, nil
+}
+
+func IncrementCounter(name string, num int64) (int64, error) {
+	var idgen MIdGen
+	filter := bson.D{{"_id", name}}
+	update := bson.D{{"$inc", bson.D{{"seq", num}}}}
+	upsert := true
+	after := options.After
+	opt := options.FindOneAndUpdateOptions{ReturnDocument: &after, Upsert: &upsert}
+	collection := gmongo.TestDB.Collection(tableName)
+	err := collection.FindOneAndUpdate(context.Background(), filter, update, &opt).Decode(&idgen)
+	//log.Println(err)
+	if err != nil {
+		return 0, err
+	}
+	return idgen.Seq, nil
 }
